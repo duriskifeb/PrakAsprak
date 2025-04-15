362cdbd99e6e74009efb2f53bb035c3ee59958d5
duriskifeb <rizkydurfeb976@gmail.com>
2025-04-15 13:03:47 +0700

    fix Praktikum

--- OTWE_UTS/Car.java
+++ OTWE_UTS/Car.java
@@ -18,6 +18,6 @@ public class Car extends Veichle{
 
     @Override
     public String toString() {
-            return super.toString() + ", Doors : " + 
+            return super.toString() + ", Doors : " + numDoors;
     }
 }
